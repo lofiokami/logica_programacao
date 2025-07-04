25a21e11827ab6c928afbc20b55542fe25774c5a
lofiokami <lofiokami@gmail.com>
2025-07-03 21:57:48 -0300

    melhoria

--- python_bd/vendas.py
+++ python_bd/vendas.py
@@ -39,26 +39,40 @@ def inserir_item_venda(conexao, item_venda):
     conexao.commit()
 
 def inserir_vendas(conexao):
+    venda = alimentar_venda(conexao)
+    itens_venda = alimentar_itens_venda()
+    total_venda = calcular_total_venda(itens_venda)
 
+    id_venda = insert_venda(conexao, venda, total_venda)
+
+    for item in itens_venda:
+        item_data = (id_venda, item['id_produto'], item['quantidade'],
+                     item['preco_unitario'], item['valor_total'])
+        inserir_item_venda(conexao,item_data)
+
+def alimentar_venda():
     id_cliente   = input ("Digite o ID do cliente:")
     data_venda   = datetime.now()
     numero_venda = input ("Digite o número da venda:")
     valor_venda  = 0
 
+    return (id_cliente, data_venda, numero_venda, valor_venda)
+
+def insert_venda(conexao, venda, valor_venda):
     cursor = conexao.cursor()
     sql_insert_venda = """
         insert into venda (id_cliente, data_venda, numero_venda, valor_venda)
         values (%s, %s, %s, %s) returning id;
     """
-    dados_vendas = (id_cliente, data_venda,numero_venda,valor_venda)
+    dados_vendas = (venda[0], venda[1], venda[2], valor_venda)
     cursor.execute(sql_insert_venda, dados_vendas)
     venda_id = cursor.fetchone()[0]
     conexao.commit()
+    return venda_id
 
-
+def alimentar_itens_venda():
     itens_venda = []
     while(True):
-         
          id_produto = int(input("Digite o ID do produto:"))
          quantidade = float(input("Digite a quantidade:"))
          preco_unitario = float(input("Digite o preço unitario:"))
@@ -75,9 +89,11 @@ def inserir_vendas(conexao):
          print(itens_venda)
          if continua == "N":
             break
-         
-    id_venda = venda_id
+    return itens_venda
+
+def calcular_total_venda (itens_venda):
+    total_venda = 0
+
     for item in itens_venda:
-        item_data = (id_venda, item['id_produto'], item['quantidade'],
-                     item['preco_unitario'], item['valor_total'])
-        inserir_item_venda(conexao,item_data)
+        total_venda = total_venda + item['valor_total']
+    return total_venda
